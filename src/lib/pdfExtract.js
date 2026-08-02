a555b8d340bfed947b4a973210121cd9c47e1cb1
Ebosetale Victor <ebosetalevictor1@gmail.com>
2026-08-02 12:30:12 +0100

    pdf extract

--- src/lib/pdfExtract.js
+++ src/lib/pdfExtract.js
@@ -1,5 +1,5 @@
 import * as pdfjsLib from 'pdfjs-dist'
-import workerUrl from 'pdfjs-dist/build/pdf.worker.min.mjs?url'
+import workerUrl from 'pdfjs-dist/build/pdf.worker.min.js?url'
 
 pdfjsLib.GlobalWorkerOptions.workerSrc = workerUrl
 
